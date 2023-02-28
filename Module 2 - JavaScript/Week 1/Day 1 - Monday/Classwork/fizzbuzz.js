b86c43a25ac65539b888d40638e6772c638e1830
justinluce <luce@midlandu.edu>
2023-02-27 19:28:27 -0600

    finishes day 1

--- Module 2 - JavaScript/Week 1/Day 1 - Monday/Classwork/fizzbuzz.js
+++ Module 2 - JavaScript/Week 1/Day 1 - Monday/Classwork/fizzbuzz.js
@@ -5,3 +5,16 @@
 // Log the number and "Fizz" if the number is divisible by 3
 // Log the number and "Buzz" if the number is divisible by 5
 // Log the number if none of the above conditions are true
+
+for (i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(`${i} FizzBuzz`);
+    } else if (i % 3 === 0) {
+        console.log(`${i} Fizz`);
+    } else if (i % 5 === 0) {
+        console.log(`${i} Buzz`);
+    } else {
+        console.log(`${i}`);
+    }
+}
+
