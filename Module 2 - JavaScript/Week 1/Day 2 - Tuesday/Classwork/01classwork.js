ef7c9e0932a4123a72b831ae5f6c625f55ef0042
justinluce <luce@midlandu.edu>
2023-02-28 17:31:00 -0600

    adds day 2

---
+++ Module 2 - JavaScript/Week 1/Day 2 - Tuesday/Classwork/01classwork.js
@@ -0,0 +1,58 @@
+//! LOOP PRACTICE
+
+// -----------------------------------------------------------------
+
+//! Take an input from the user. Starting with the 4th character,
+//! log all characters until the end of the input.
+//! If the input is shorter than 4 characters, nothing should be logged.
+
+// -----------------------------------------------------------------
+
+//! Pick a random number. Prompt the user to guess a number.
+//! If the number is correct, end the loop and tell the user how many tries it took to guess.
+//! If it is incorrect, continue the loop.
+//! (Test functionality by logging the number that was randomly generated in the prompt).
+
+// To get a random number: Math.floor(Math.random() * max) + 1;
+// Math.random generates a random number between 0 (inclusive) and 1 (exclusive).
+// We multiply that number by the max value you want.
+// Math.floor always rounds the number down.
+// If max = 10, it should generate a random number from 1 to 10.
+
+
+// -----------------------------------------------------------------
+
+//! Using nested for loops, create and log the following pattern:
+/*
+//!    *
+//!    **
+//!    ***
+//!    ****
+//!    *****
+*/
+
+// -----------------------------------------------------------------
+
+//! Simulate a coin flip. Start a counter at 0. If the initial flip was heads,
+//! leave the counter at 0 and log: "It took 0 retries to get heads!".
+//! If the coin was tails, try again and keep doing so until heads happens. Log the amount of retries it took.
+
+// COMPARISON PRACTICE
+// -----------------------------------------------------------------
+
+//! Build a simple site that prompts a user for their first name and then a number
+//! between one and one-hundred (inclusive).
+//!  - Tell them whether their number is odd or even and if its greater than,
+//!    less than, or equal to 50 and then log those messages separately to the console.
+//!  - Log every number before theirs and every number from 100 counting down to theirs in two separate loops.
+//!  - If their name is your name send an alert saying that it is a great name.
+
+// Use toLowerCase() method to make their name entirely lower case.
+
+// -----------------------------------------------------------------
+
+//! Take the following arrays: `[-1,-2,2,10,7,8]` and `[4,-2,2,7,9,5]`
+//! and see how many items the two arrays share in common.
+//! Do this without prototypical methods (don't use array.filter and array.includes).
+//! You will need a nested loop.
+
