c13420784c397a40c5cdcad9f066d606aaeee6fd
justinluce <luce@midlandu.edu>
2023-02-28 20:36:35 -0600

    finalizes solutions

--- Module 2 - JavaScript/Week 1/Day 2 - Tuesday/Classwork/01classwork.js
+++ Module 2 - JavaScript/Week 1/Day 2 - Tuesday/Classwork/01classwork.js
@@ -48,13 +48,13 @@
 
 */
 
-for (let i = 1; i <= 5; i++) {
-    let row = "";
-    for (let j = 1; j <= i; j++) {
-        row += "*";
-    }
+// for (let i = 1; i <= 5; i++) {
+//     let row = "";
+//     for (let j = 1; j <= i; j++) {
+//         row += "*";
+//     }
     // console.log(row);
-}
+// }
 
 // -----------------------------------------------------------------
 
@@ -62,16 +62,23 @@ for (let i = 1; i <= 5; i++) {
 //! leave the counter at 0 and log: "It took 0 retries to get heads!".
 //! If the coin was tails, try again and keep doing so until heads happens. Log the amount of retries it took.
 
-let counter = -1;
-let coin;
+// let counter = -1;
+// let coin;
 
-// 0 is heads and 1 is tails
+// // 0 is heads; 1 is tails
 
-// Put this in a loop
-coin = Math.floor(Math.random() * 2);
-// This will generate a number between 0 and 1;
-// The highest number Math.random() can generate is .99,
-// So, if it generates .99, we multiply it by 2 to get 1.98, which gets rounded down to 1 by Math.floor()
+// while (coin !== 0) {
+//     counter++;
+//     coin = Math.floor(Math.random() * 2); //This will generate a number between 0 and 1;
+//     // Explanation:
+//     // The highest number Math.random() can generate is .99,
+//     // So, if it generates .99, we multiply it by 2 to get 1.98, which gets rounded down to 1 by Math.floor()
+//     // Any other number from 0 to .98 will also get multiplied by 2 and rounded down,
+//     // and those numbers will always be between 0 and 1
+// }
+
+// console.log(`It took ${counter} retries to get heads!`);
+
 
 // COMPARISON PRACTICE
 // -----------------------------------------------------------------
@@ -85,6 +92,33 @@ coin = Math.floor(Math.random() * 2);
 
 // Use toLowerCase() method to make their name entirely lower case.
 
+// const firstName = prompt("What is your first name?");
+// const userNum = prompt("Give me a number between 1 - 100");
+
+// if (userNum % 2 !== 0) {
+//   console.log("Your number is odd");
+// } else {
+//   console.log("Your number is even");
+// }
+
+// if (userNum <= 50) {
+//   console.log("Your number is less than or equal to 50");
+// } else {
+//   console.log("Your number is greater than 50");
+// }
+
+// for (let i = 1; i <= userNum; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 100; i >= userNum; i--) {
+//   console.log(i);
+// }
+
+// if (firstName.toLowerCase() === "justin") {
+//   alert("Great name!");
+// }
+
 // -----------------------------------------------------------------
 
 //! Take the following arrays: `[-1,-2,2,10,7,8]` and `[4,-2,2,7,9,5]`
@@ -92,3 +126,23 @@ coin = Math.floor(Math.random() * 2);
 //! Do this without prototypical methods (don't use array.filter and array.includes).
 //! You will need a nested loop.
 
+// let arr1 = [-1,-2,2,10,7,8];
+// let arr2 = [4,-2,2,7,9,5];
+// let common = 0;
+
+// for (let i = 0; i < arr1.length; i++) {
+//     for (let j = 0; j < arr2.length; j++) {
+//         if (arr1[i] === arr2[j]) {
+//             common++;
+//         }
+//     }
+// }
+
+// console.log(`There are ${common} items in common`);
+
+// With prototypical methods
+// arr1.array.forEach(element => {
+//     if (arr2.includes(element)) {
+//         common++;
+//     }
+// });
